3aa8c18b7a75046310740584c5cf7db1be08fd84
Paschalis Papanikolaou <pashalispap@gmail.com>
2014-11-18 19:38:32 +0200

    Coursera - R-Programming - Assigment2
    The cachematrix.R program for the assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,49 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+I<-NULL
 
+set<-function(matrix){
+  A<<-matrix
+  I<<-NULL
+}
+
+get<-function(){
+  A
+}
+
+setInverse<-function(inverse){
+  I<<-inverse
+}
+
+getInverse<-function(){
+  I
+}
+
+list(set=set, get=get,
+     setInverse=setInvere,
+     getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  
         ## Return a matrix that is the inverse of 'x'
+  i<-x$getInverse
+  
+  if(!is.null(i)){
+    message("getting cached data")
+    return (i)
+  }
+  
+  data<-x$get()
+  
+  i<-solve(data) %*% data
+  
+  x$setInverse(i)
+  
+  i
 }
